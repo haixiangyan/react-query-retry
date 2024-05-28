11559f19c4b077b35c3df8e987052571ae9f4089
haixiangyan <haixiang6123@gmail.com>
2024-05-29 00:39:05 +0800

    feat: 初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import {useState} from 'react'
+import './App.css'
+
+interface QueryResult {
+  id: string;
+  text: string;
+}
+
+interface ListItem {
+  id: string;
+  name: string;
+}
+
+let counter = 0;
+
+const fetchList = (): Promise<ListItem[]> => {
+  console.log('start fetch...')
+  counter += 1;
+  // 成功
+  if (counter > 3) {
+    return new Promise(resolve => {
+      counter = 1;
+
+      setTimeout(() => {
+        console.log('fetch success!')
+        resolve([
+          {
+            id: '1',
+            name: '1',
+          },
+          {
+            id: '2',
+            name: '2',
+          },
+        ]);
+      }, 2000);
+    })
+  }
+
+  // 失败
+  return new Promise((_, reject) => {
+    setTimeout(() => {
+      console.log('fetch error!')
+      reject('timeout');
+    }, 2000);
+  })
+}
+
+const pushResults = (results: QueryResult[], text: string): QueryResult[] => {
+  return [
+    ...results,
+    {
+      id: String(results.length + 1),
+      text,
+    }
+  ]
+}
+
+function App() {
+  const [results, setResults] = useState<QueryResult[]>([]);
+
+  const onQuery = async () => {
+    // Results 有问题
+    console.log('ressults', results);
+    try {
+      const list = await fetchList();
+
+      setResults(pushResults(results, JSON.stringify(list)));
+    } catch (e) {
+      const x = pushResults(results, String(e))
+
+      setResults(x);
+
+      await onQuery();
+    }
+  }
+
+  return (
+    <div>
+      <button onClick={onQuery}>Query</button>
+
+      <ul>
+        {results.map(result => (
+          <li key={result.id}>{result.text}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default App
